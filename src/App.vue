fad4d46576a7bddfd29b6974a1a31084b0b123f3
Konstantin Meshkov <meshkovkm@giskaluga.ru>
2021-08-18 17:16:59 +0300

    feat: add filter & page to url

--- src/App.vue
+++ src/App.vue
@@ -322,6 +322,18 @@ export default {
     };
   },
   created: function () {
+    const windowData = Object.fromEntries(
+      new URL(window.location).searchParams.entries()
+    );
+
+    if (windowData.filter) {
+      this.filter = windowData.filter;
+    }
+
+    if (windowData.page) {
+      this.page = windowData.page;
+    }
+
     const tickersData = localStorage.getItem("cryptonomicon-list");
 
     if (tickersData) {
@@ -473,6 +485,19 @@ export default {
   watch: {
     filter() {
       this.page = 1;
+      window.history.pushState(
+        null,
+        document.title,
+        `${window.location.pathname}?filter=${this.filter}&page=${this.page}`
+      );
+    },
+
+    page() {
+      window.history.pushState(
+        null,
+        document.title,
+        `${window.location.pathname}?filter=${this.filter}&page=${this.page}`
+      );
     },
   },
 };
