a3b985b8dd1ce404903492bf192a9f76c46a1f62
Konstantin Meshkov <meshkovkm@giskaluga.ru>
2021-08-18 11:17:48 +0300

    feat: saving to localstorage

--- src/App.vue
+++ src/App.vue
@@ -262,9 +262,30 @@ export default {
     };
   },
   created: function () {
+    const tickersData = localStorage.getItem("cryptonomicon-list");
+
+    if (tickersData) {
+      this.tickers = JSON.parse(tickersData);
+      this.tickers.forEach((ticker) => this.subscribeToUpdates(ticker.name));
+    }
+
     this.getTickers();
   },
   methods: {
+    subscribeToUpdates(tickerName) {
+      setInterval(async () => {
+        const f = await fetch(
+          `https://min-api.cryptocompare.com/data/price?fsym=${tickerName}&tsyms=USD&api_key=9a54c61b45ac389b252ee8b8eab0a0521930b18780641228922688851af95c78`
+        );
+        const data = await f.json();
+        this.tickers.find((t) => t.name == tickerName).price =
+          data.USD > 1 ? data.USD.toFixed(2) : data.USD.toPrecision(2);
+        if (this.sel?.name == tickerName) {
+          this.graph.push(data.USD);
+        }
+      }, 3000);
+    },
+
     add() {
       const currentTicker = { name: this.ticker.toUpperCase(), price: "-" };
 
@@ -279,17 +300,11 @@ export default {
       }
 
       this.tickers.push(currentTicker);
-      setInterval(async () => {
-        const f = await fetch(
-          `https://min-api.cryptocompare.com/data/price?fsym=${currentTicker.name}&tsyms=USD&api_key=9a54c61b45ac389b252ee8b8eab0a0521930b18780641228922688851af95c78`
-        );
-        const data = await f.json();
-        this.tickers.find((t) => t.name == currentTicker.name).price =
-          data.USD > 1 ? data.USD.toFixed(2) : data.USD.toPrecision(2);
-        if (this.sel?.name == currentTicker.name) {
-          this.graph.push(data.USD);
-        }
-      }, 3000);
+
+      localStorage.setItem("cryptonomicon-list", JSON.stringify(this.tickers));
+
+      this.subscribeToUpdates(currentTicker.name);
+
       this.ticker = "";
       this.tickersAutocompete = [];
     },
